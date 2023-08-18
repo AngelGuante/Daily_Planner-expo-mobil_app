4d1148647ecf9099105b1992cbaf83206166d8a2
Miguel Angel Guante Suero <49294128+AngelGuante@users.noreply.github.com>
2023-08-17 22:47:25 -0400

    Adding README.md

---
+++ Components/Pages/Welcome.jsx
@@ -0,0 +1,27 @@
+import { View, Text } from 'react-native'
+
+const Welcome = () =>
+    <View>
+        <Text>Welcome page</Text>
+    </View>
+
+export default Welcome
+
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>DEMO!</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
---
+++ README.md
@@ -0,0 +1,15 @@
+## App Colors 
+```
+https://realtimecolors.com/?colors=0a0a0b-ffffff-f78548-cbb6f1-5419c1
+```
+
+## Assets
+```
+https://undraw.co/
+```
+
+## References
+```
+https://www.behance.net/gallery/101376559/Signup-Login-Screen-UI-Design
+https://dribbble.com/shots/5770976-To-Do-Task-list-App/attachments/1244890?mode=media
+```
